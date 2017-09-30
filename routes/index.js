67860da861bfba8c10f54608134680a526c7e6fa
Jixing Li <724667364@qq.com>
2017-09-30 13:23:02 -0700

    Add gitignore file

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team: NOP' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Team: NOP' });
+// });
+
+router.get('/', function(req, res) {
+    res.sendfile('./views/feedback.html');
 });
 
 module.exports = router;
