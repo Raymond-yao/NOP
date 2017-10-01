66b08317dbe749fa3ce32800c208e3eb69b7c1e4
ridum <ridum1996@gmail.com>
2017-10-01 01:19:10 -0700

    form input

--- public/react/accInput.js
+++ public/react/accInput.js
@@ -1,3 +1,32 @@
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit="">
+                    <div className="row">
+                        <div className="col-xs-4">
+                            <label>aaClass name</label>
+                            <input type="text" name="class" maxLength="200" autoFocus className="form-control" ></input>
+                        </div>
+                        <div className="col-xs-4">
+                            <label>date</label>
+                            <input type="date" name="date" maxLength="200" autoFocus className="form-control" ></input>
+                        </div>
+                        <div className="col-xs-4">
+                            <label>Detail</label>
+                            <input type="text" name="class" maxLength="200" autoFocus className="form-control" ></input>
+                        </div>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello, world!
-    </h1>, document.getElementById('root'));
+    <Reservation />, document.getElementById('root'));
