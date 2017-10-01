b49eb907dda0f2952fecbbeee18f5154287c6000
ridum <ridum1996@gmail.com>
2017-10-01 02:39:48 -0700

    wtf

--- public/assets/feedback.js
+++ public/assets/feedback.js
@@ -1,16 +1,22 @@
-$(function(){
+$(function () {
+	$.ajax({
+		type: "get", url: "/getJSON", dataType: "json", success: function (response) {
 
-	for (var i = 0 ; i<3;i++){
-		var newDiv = document.createElement("div");
-		newDiv.classList.add("check-item");
-		$(newDiv).attr("id",i.toString());
-		var newSpan = document.createElement("span");
-		var textnode = document.createTextNode("haha");
-		newSpan.classList.add("knowledge-point");
-		newSpan.appendChild(textnode);
-		newDiv.appendChild(newSpan);
-		$(".knowledge-checkbox-container").append(newDiv);
-		$("#"+i.toString() ).append('<label class="switch checkbox"><input type="checkbox" checked><span class="slider round"></span></label>' );
-}
+			for (var i = 0; i < 3; i++) {
+				var newDiv = document.createElement("div");
+				newDiv.classList.add("check-item");
+				$(newDiv).attr("id", i.toString());
+				var newSpan = document.createElement("span");
+				var textnode = document.createTextNode("haha");
+				newSpan.classList.add("knowledge-point");
+				newSpan.appendChild(textnode);
+				newDiv.appendChild(newSpan);
+				$(".knowledge-checkbox-container").append(newDiv);
+				$("#" + i.toString()).append('<label class="switch checkbox"><input type="checkbox" checked><span class="slider round"></span></label>');
+			}
+		}, error: function (response) { console.log(response); }
+	});
+
+
 });
 
