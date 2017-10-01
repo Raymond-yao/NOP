ce6b5f3a743d74bf2ec6c022a7bd55469130388c
ridum <ridum1996@gmail.com>
2017-10-01 01:44:37 -0700

    hahahahaleo sb

--- public/react/accInput.js
+++ public/react/accInput.js
@@ -5,25 +5,23 @@ class Reservation extends React.Component {
 
     render() {
         return (
-            <div>
-                <form action="/test" method="post">
-                    <div className="row">
-                        <div className="col-xs-4">
-                            <label>aaClass name</label>
-                            <input type="text" name="class" maxLength="200" autoFocus className="form-control" ></input>
-                        </div>
-                        <div className="col-xs-4">
-                            <label>date</label>
-                            <input type="date" name="date" maxLength="200" autoFocus className="form-control" ></input>
-                        </div>
-                        <div className="col-xs-4">
-                            <label>Detail</label>
-                            <input type="text" name="detail" maxLength="200" autoFocus className="form-control" ></input>
-                        </div>
+            <form action="/test" method="post">
+                <div className="row">
+                    <div className="col-xs-4">
+                        <label>aaClass name</label>
+                        <input type="text" name="class" maxLength="200" autoFocus className="form-control" ></input>
                     </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+                    <div className="col-xs-4">
+                        <label>date</label>
+                        <input type="date" name="date" maxLength="200" autoFocus className="form-control" ></input>
+                    </div>
+                    <div className="col-xs-4">
+                        <label>Detail</label>
+                        <input type="text" name="detail" maxLength="200" autoFocus className="form-control" ></input>
+                    </div>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         );
     }
 }
